50e833d8dc1dbc7c0aed6a995c1c1aa748fe51af
Ang Ming Liang <angmingliang4017ic@gmail.com>
2017-12-25 09:05:01 +0800

    updated

--- CNN_with_PowerSign.py
+++ CNN_with_PowerSign.py
@@ -16,7 +16,7 @@ torch.manual_seed(1)
 writer = SummaryWriter()
 
 # Hyper Parameters
-EPOCH = 500
+EPOCH = 3x00
 BATCH_SIZE = 128
 LR = 0.01
 
--- CNN_with_adam.py
+++ CNN_with_adam.py
@@ -17,7 +17,7 @@ torch.manual_seed(1)
 writer = SummaryWriter()
 
 # Hyper Parameters
-EPOCH = 500
+EPOCH = 300
 BATCH_SIZE = 128
 LR = 0.001
 
